526950e81d21f011e4f862190097739220400b6a
Your GitHub Username <your_github_email@example.com>
2026-05-19 10:36:10 +0530

    Fixed uploads folder issue

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+import os
+
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route('/')
 def home():
